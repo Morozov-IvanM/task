6155ad21365cc7ba572d78cded93716bc46d2e11
Morozov-IvanM <70146068+Morozov-IvanM@users.noreply.github.com>
2020-09-08 14:34:49 +0400

    Add files via upload

---
+++ hello.rb
@@ -0,0 +1,5 @@
+puts 'How match second do y live?'
+time=gets.chomp
+ear=time.to_i/60/60/24/365
+ear1=ear.to_s
+puts 'Y live '+ear1+' ear'
---
+++ number.rb
@@ -0,0 +1,5 @@
+puts 'Hello, what is your favorite number? '
+num=gets.chomp
+num=num.to_i+1
+num=num.to_s
+puts 'may be '+num+' - is better number?'
