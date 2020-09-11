0d44d7a848ed4ab92370442b2e9e485c9a2d0c04
Morozov-IvanM <70146068+Morozov-IvanM@users.noreply.github.com>
2020-09-11 10:55:04 +0400

    random was added to the Babka file

--- babka.rb
+++ babka.rb
@@ -1,11 +1,13 @@
 command = ''
-while command !='Пока'
+puts 'Hi'
+while command !='Buy'
   command = gets.chomp
-if command==command.upcase
-  then
-   puts 'НЕТ,НИ РАЗУ С' +rand(1930..1950).to_s+ ' ГОДА! '
-  else
-   puts 'АСЬ?!ГОВОРИ ГРОМЧЕ,ВНУЧЕК!'
+  if command=='Buy' then puts 'Пока внучек' else
+      if command==command.upcase
+      then
+        puts 'НЕТ,НИ РАЗУ С' +rand(1930..1950).to_s+ ' ГОДА! '
+      else
+        puts 'АСЬ?!ГОВОРИ ГРОМЧЕ,ВНУЧЕК!'
+      end
+    end
 end
-end
-puts 'Пока внучек'
